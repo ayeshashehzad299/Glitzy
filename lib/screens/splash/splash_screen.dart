d5b4b61a998384163b3b3dd5165099f472b54867
ayesha <ayeshashehzad299@gmail.com>
2025-11-09 11:58:47 +0500

    Add polished splash screen with logo, tagline, and layout improvements

---
+++ lib/screens/splash/splash_screen.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  static const Color blushPink = Color.fromARGB(255, 253, 240, 243);
+  static const Color textPurple = Color(0xFF5C54A7);
+
+  // @override
+  // void initState() {
+  //   super.initState();fl
+  //   Timer(const Duration(seconds: 3), () {
+  //     Navigator.pushReplacement(context,
+  //         MaterialPageRoute(builder: (context) => const OnboardingScreen1()));
+  //   });
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final logoTopPosition = screenHeight * 0.20;
+    const logoSize = 320.0;
+
+    return Scaffold(
+      backgroundColor: blushPink,
+      body: Stack(
+        children: [
+          Positioned(
+            top: logoTopPosition,
+            left: 0,
+            right: 18,
+            child: Center(
+              child: Image.asset(
+                'assets/images/logo.png',
+                width: logoSize,
+                height: logoSize,
+              ),
+            ),
+          ),
+          Positioned(
+            top: logoTopPosition + 280,
+            left: 0,
+            right: 0,
+            child: Center(
+              child: Text(
+                'Where Conversations Sparkle',
+                style: GoogleFonts.chilanka(
+                  color: textPurple,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
