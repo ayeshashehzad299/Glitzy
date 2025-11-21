df8fba1eeabb443cfe89876d8d0c235055a096d9
ayesha <ayeshashehzad299@gmail.com>
2025-11-21 14:17:54 +0500

    add Friends screen AppBar UI with notification icon placeholder

--- lib/screens/friends/friends_screen.dart
+++ lib/screens/friends/friends_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class FriendsScreen extends StatefulWidget {
   const FriendsScreen({super.key});
@@ -8,8 +10,49 @@ class FriendsScreen extends StatefulWidget {
 }
 
 class _FriendsScreenState extends State<FriendsScreen> {
+  static const Color blushPink = Color(0xFFFFF4F7);
+  static const Color hotPink = Color(0xFFFF69B4);
+  static const Color darkText = Color(0xFF8E2A6C);
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: blushPink,
+      appBar: AppBar(
+        backgroundColor: blushPink,
+        elevation: 0,
+        centerTitle: false,
+        toolbarHeight: 80,
+        titleSpacing: 30,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 20),
+            child: IconButton(
+              onPressed: () {},
+              icon: Container(
+                padding: const EdgeInsets.all(7),
+                decoration: BoxDecoration(
+                  border: Border.all(color: hotPink, width: 1.2),
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.white,
+                ),
+                child: Icon(Icons.notifications_none, color: hotPink),
+              ),
+              tooltip: 'Notifications',
+            ),
+          ),
+        ],
+        title: Padding(
+          padding: const EdgeInsets.only(top: 6.0),
+          child: Text(
+            'Friends',
+            style: GoogleFonts.chilanka(
+              color: hotPink,
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/screens/friends/invite_friends_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InviteFriendsScreen extends StatefulWidget {
+  const InviteFriendsScreen({super.key});
+
+  @override
+  State<InviteFriendsScreen> createState() => _InviteFriendsScreenState();
+}
+
+class _InviteFriendsScreenState extends State<InviteFriendsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
