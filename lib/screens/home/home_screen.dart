d627784ecec731cfa4c56c136504c2f20e4079ae
ayesha <ayeshashehzad299@gmail.com>
2025-12-11 23:38:28 +0500

    implement custom AppBar for Chat Screen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -13,6 +14,26 @@ class _HomeScreenState extends State<HomeScreen> {
   static const Color darkText = Color(0xFF8E2A6C);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: blushPink);
+    return Scaffold(
+      backgroundColor: blushPink,
+      appBar: AppBar(
+        backgroundColor: blushPink,
+        elevation: 0,
+        centerTitle: false,
+        toolbarHeight: 80,
+        titleSpacing: 30,
+        title: Padding(
+          padding: const EdgeInsets.only(top: 6.0),
+          child: Text(
+            'Chats',
+            style: GoogleFonts.chilanka(
+              color: hotPink,
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
